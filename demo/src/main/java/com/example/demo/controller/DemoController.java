838accbcfa78c30e4393d7eda51a37b17876b109
Prashanth-chinnla <chinnalaprashanth@gmail.com>
2023-03-07 19:49:49 +0530

    checking pre-commit

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "demo";
+	}
+}
