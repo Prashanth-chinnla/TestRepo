88e742d5747fd3c016a5728d32459a5ef6bdcb11
Prashanth-chinnla <chinnalaprashanth@gmail.com>
2023-03-08 17:04:39 +0530

    test commit

--- demo/src/main/java/com/example/demo/Example.java
+++ demo/src/main/java/com/example/demo/Example.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class Example {
 
 	public boolean compare(String firstList[], String secondList[]) {
+		System.out.println("kkk");
 		return firstList.equals(secondList);
 	}
 }
